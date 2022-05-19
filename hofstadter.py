680bce0f3c1716183dfa98d728aee2097239b5b7
Ziang Wang <ziangwang9@zju.edu.cn>
2022-05-20 00:20:59 +0800

    Fix a bug
    - mismatch between magnetic field and period boundary

--- hofstadter.py
+++ hofstadter.py
@@ -68,29 +68,30 @@ class Hofstadter(TightBinding):
         self.q = q
         self.vpotential = magnetic.vpotential
         self.geometry = geometry
-
         self.hamiltonian_k = 0
-        # initialize a Hilbert Space in momentum space
-        self.hamiltonian_k = self.eps / 2 * qt.qeye(self.dim)
-        # present lattice in fock basis
-        # forward hopping
-        for n in range(self.dim[0]):
-            for m in range(self.dim[1]):
-                if m + n == sum(self.dim) - 2:
-                    continue
-                elif m == self.dim[1] - 1 and n < self.dim[0] - 1:
-                    self.hamiltonian_k += -self.t * self.hopping(
-                        [n, m], [n + 1, m], self.alpha, self.q, self.vpotential)
-                elif n == self.dim[0] - 1 and m < self.dim[1] - 1:
-                    self.hamiltonian_k += -self.t * self.hopping(
-                        [n, m], [n, m + 1], self.alpha, self.q, self.vpotential)
-                else:
-                    self.hamiltonian_k += -self.t * self.hopping(
-                        [n, m], [n, m + 1], self.alpha, self.q, self.vpotential)
-                    self.hamiltonian_k += -self.t * self.hopping(
-                        [n, m], [n + 1, m], self.alpha, self.q, self.vpotential)
-        # conjugate hopping
-        self.hamiltonian_k += self.hamiltonian_k.dag()
+        if self.geometry in ['torus', 'cylinder', 'identity']:
+
+            # initialize a Hilbert Space in momentum space
+            self.hamiltonian_k = self.eps / 2 * qt.qeye(self.dim)
+            # present lattice in fock basis
+            # forward hopping
+            for n in range(self.dim[0]):
+                for m in range(self.dim[1]):
+                    if m + n == sum(self.dim) - 2:
+                        continue
+                    elif m == self.dim[1] - 1 and n < self.dim[0] - 1:
+                        self.hamiltonian_k += -self.t * self.hopping(
+                            [n, m], [n + 1, m], self.alpha, self.q, self.vpotential)
+                    elif n == self.dim[0] - 1 and m < self.dim[1] - 1:
+                        self.hamiltonian_k += -self.t * self.hopping(
+                            [n, m], [n, m + 1], self.alpha, self.q, self.vpotential)
+                    else:
+                        self.hamiltonian_k += -self.t * self.hopping(
+                            [n, m], [n, m + 1], self.alpha, self.q, self.vpotential)
+                        self.hamiltonian_k += -self.t * self.hopping(
+                            [n, m], [n + 1, m], self.alpha, self.q, self.vpotential)
+            # conjugate hopping
+            self.hamiltonian_k += self.hamiltonian_k.dag()
 
     def transform_to_momentum_space(self, k_point):
         """2-d Discrete Fourier Transfrom.
@@ -134,31 +135,54 @@ class Hofstadter(TightBinding):
             #             (real_index - displacement), k_point)))
             # dft_matrix = np.asarray(dft_matrix)
 
-            unit_vec = np.array([1, self.dim[1]])
             # initialize a Hilbert Space in reciprocal space
             h_k = self.eps / 2 * qt.qeye(self.dim[1])
             # hopping
             for i in range(self.dim[1]):
                 h_k += -self.t * np.exp(
-                    -2j * np.pi * k_point[0] / self.k_vec[0] *
+                    -2j * np.pi * k_point[0] *
+                    self.r_vec[0]) * self.hopping([0, i], [1, i],
+                                                self.alpha,
+                                                self.q,
+                                                self.vpotential,
+                                                hopping_1d=True)
+                if i != self.dim[1] - 1:
+                    h_k += -self.t * np.exp(
+                        -2j * np.pi * k_point[1] * self.r_vec[1]) * self.hopping(
+                            [0, i], [0, i + 1],
+                            self.alpha,
+                            self.q,
+                            self.vpotential,
+                            hopping_1d=True)
+            h_k += -self.t * np.exp(
+                -2j * np.pi * k_point[1] * self.r_vec[1]) * self.hopping(
+                    [0, self.dim[1] - 1], [0, 0],
+                    self.alpha,
+                    self.q,
+                    self.vpotential,
+                    hopping_1d=True)
+            h_k += h_k.dag()
+
+        if self.geometry == 'cylinder_1d':
+            unit_vec = np.array([1, 3])
+            # initialize a Hilbert Space in reciprocal space
+            h_k = self.eps / 2 * qt.qeye(self.dim[1])
+            # hopping
+            for i in range(self.dim[1]):
+                h_k += -self.t * np.exp(
+                    -2j * np.pi * k_point[0] *
                     unit_vec[0]) * self.hopping([0, i], [1, i],
                                                 self.alpha,
                                                 self.q,
                                                 self.vpotential,
                                                 hopping_1d=True)
                 if i != self.dim[1] - 1:
-                    h_k += -self.t * self.hopping([0, i], [0, i + 1],
-                                                  self.alpha,
-                                                  self.q,
-                                                  self.vpotential,
-                                                  hopping_1d=True)
-            h_k += -self.t * np.exp(
-                -2j * np.pi * k_point[1] * unit_vec[1]) * self.hopping(
-                    [0, self.dim[1] - 1], [0, 0],
-                    self.alpha,
-                    self.q,
-                    self.vpotential,
-                    hopping_1d=True)
+                    h_k += -self.t * self.hopping(
+                            [0, i], [0, i + 1],
+                            self.alpha,
+                            self.q,
+                            self.vpotential,
+                            hopping_1d=True)
             h_k += h_k.dag()
 
         if self.geometry == 'identity':
@@ -224,8 +248,13 @@ class Hofstadter(TightBinding):
         k_x = np.linspace(-0.5, 0.5, grid)
         k_list = np.array([[k_y, k_xm] for k_xm in k_x])
         eig_list = self.solve_klist(k_list)
-        plt.plot(k_x, eig_list)
-        plt.show()
+        for eig in eig_list.T:
+            plt.scatter(k_x, eig, edgecolors='none', s=0.6, c='k')
+        plt.xlabel('$k_x$')
+        plt.xticks([-0.5, -1 / 3, -1 / 6, 0, 1 / 6, 1 / 3, 0.5],
+                   ['$-\pi$', '$-2\pi/3$', '$-\pi/3$', 0, '$\pi/3$', '$2\pi/3$', '$\pi$'])
+        plt.ylabel('Energy E')
+        plt.yticks([-3, -2, -1, 0, 1, 2, 3], ['-3t', '-2t', '-1t', 0, '1t', '2t', '3t'])
 
     def calc_band_dispersion_y(self, k_x=0, grid=50):
         """Calculate eigenenergy and eigenvector in momentum space along x axis.
@@ -234,20 +263,24 @@ class Hofstadter(TightBinding):
         k_y = np.linspace(-0.5, 0.5, grid)
         k_list = np.array([[k_yn, k_x] for k_yn in k_y])
         eig_list = self.solve_klist(k_list)
-        plt.plot(k_x, eig_list)
+        plt.scatter(k_list, eig_list, edgecolors='none', s=0.6, c='k')
         plt.show()
 
 
-def draw_hofstadter_butterfly(k_point, num_orbital, num_phi):
+def draw_hofstadter_butterfly(k_point, num_phi,  max_num_orbital=1000):
     """Draw a Hofstadter's butterfly.
     """
+
+    from fractions import Fraction
+
     energy_list = []
     phi_list = np.linspace(0, 1, num_phi)
-    for phi in phi_list:
+    phi_denominator = [Fraction(phi).limit_denominator(max_num_orbital).denominator for phi in phi_list]
+    for i, phi in enumerate(phi_list):
         mag = Magnetic([0, 0, phi])
         mag.landau_gauge('y')
-        hof = Hofstadter(16,
-                         num_orbital,
+        hof = Hofstadter(1,
+                         phi_denominator[i],
                          0,
                          1,
                          alpha=1,
@@ -256,24 +289,40 @@ def draw_hofstadter_butterfly(k_point, num_orbital, num_phi):
                          geometry='torus_1d')
         energy_list.append(hof.solve_klist(k_point).squeeze())
     energy_list = np.asarray(energy_list)
-    plt.plot(phi_list, energy_list, '.')
+    for i, phi in enumerate(phi_list):
+        if energy_list[i].ndim == 0:
+            plt.scatter(energy_list[i], phi, edgecolors='none', s=0.6, c='k')
+        else:
+            for energy in energy_list[i]:
+                plt.scatter(energy, phi, edgecolors='none', s=0.6, c='k')
+    y_lines = [0, 1 / 4, 2 / 6, 1 / 2, 4 / 6, 3 / 4, 1]
+    x_bound = [[-4, -3, -3, -3, -3, -3 , -4],
+               [4, 3, 3, 3, 3, 3, 4] ]
+    plt.hlines(y_lines, x_bound[0], x_bound[1], ['b', 'c', 'm', 'y', 'm', 'c', 'b'])
+    plt.xlabel('Energy E')
+    plt.xticks([-4,0,4], ['-4t', 0, '4t'])
+    plt.ylabel('Flux $\phi$')
+    plt.yticks(y_lines,[ 0, '$\pi/2$', '$2\pi/3$',
+                '$\pi$', '$4\pi/3$', '$3\pi/2$', '$2\pi$'])
     plt.show()
 
 
 # %%
 mag = Magnetic([0, 0, 1 / 3])
 mag.landau_gauge('y')
-hof = Hofstadter(9, 3, 0, 1, alpha=1, q=1, magnetic=mag, geometry='cylinder')
+hof = Hofstadter(1, 3, 0, 1, alpha=1, q=1, magnetic=mag, geometry='torus_1d')
 # hof.add_periodic_bound()
 hof.hamiltonian_k
 # %%
 hof.hamiltonian_k * qt.basis(hof.dim, [0, 0])
 # %%
-hof.transform_to_momentum_space([0, 0])
+hof.transform_to_momentum_space([0, 0.1])
 # %%
 hof.calc_band_dispersion_2d()
 # %%
-hof.calc_band_dispersion_x()
+for x in np.linspace(-0.5, 0.5, 20):
+    hof.calc_band_dispersion_x(x)
+plt.show()
 # %%
-draw_hofstadter_butterfly([0, 0], 30, 100)
+draw_hofstadter_butterfly([0, 0], 100)
 # %%
