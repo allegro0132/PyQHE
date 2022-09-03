9dd03d3a85325081d3f95b0b65d43ba44ded28a4
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2022-09-03 17:44:45 +0800

    Add n-d fermi level function

--- pyqhe/utility/fermi.py
+++ pyqhe/utility/fermi.py
@@ -4,10 +4,13 @@ from scipy import optimize
 import pyqhe.utility.constant as const
 
 
-def calc_meff_state(grid, wave_function, cb_meff):
-    """Calculate subband effective mass."""
-    wave_function = np.asarray(wave_function)
-    return 1.0 / np.trapz(wave_function**2 / cb_meff, x=grid, axis=1)
+def calc_meff_state(grid, wave_function: np.ndarray, cb_meff: np.ndarray):
+    """Calculate subband(specific energy levels) effective mass."""
+    meff_state = wave_function * np.conj(wave_function) * cb_meff
+    for grd in grid[::1]:
+        meff_state = np.trapz(meff_state, x=grd)
+
+    return meff_state / (const.hbar**2 * np.pi)
 
 
 class FermiStatistic:
@@ -21,16 +24,30 @@ class FermiStatistic:
     def __init__(
         self,
         grid: np.ndarray,
-        cb_meff,
+        cb_meff: np.ndarray,
         doping: np.ndarray,
     ) -> None:
-        self.doping = doping
+
+        if not isinstance(grid, list):  # 1d grid
+            grid = [grid]
         self.grid = grid
-        self.cb_meff = cb_meff
+        self.dim = [grid_axis.shape[0] for grid_axis in self.grid]
+        # check matrix dim
+        if cb_meff.shape == tuple(self.dim) or len(self.dim) == 1:
+            self.cb_meff = cb_meff
+        else:
+            raise ValueError('The dimension of cb_meff is not match')
+        if doping.shape == tuple(self.dim) or len(self.dim) == 1:
+            self.doping = doping
+        else:
+            raise ValueError('The dimension of doping is not match')
+
         # Calculate 2d doping density
         # integrate 3-d density along axis z
         # in discrete method, just sum over r'n_3d[i] * (grid[i + 1] - grid[i])'
-        self.max_occupation = np.trapz(doping, grid)
+        self.max_occupation = doping
+        for grid in self.grid[::-1]:
+            self.max_occupation = np.trapz(self.max_occupation, grid)
         # Cache parameters
         self.fermi_energy = None
         self.n_states = None
@@ -49,25 +66,24 @@ class FermiStatistic:
         self.meff_state = calc_meff_state(self.grid, wave_function,
                                           self.cb_meff)
         # list all fermi energy candidate
-        candidate_fermi_energy = []
+        estimate_fermi_energy = []
         for i, _ in enumerate(eig_val):
             accumulate_energy = np.sum(eig_val[:i + 1])
-            candidate_fermi_energy.append(
-                (self.max_occupation * const.hbar**2 * np.pi /
-                 self.meff_state[i] + accumulate_energy) / (i + 1))
+            estimate_fermi_energy.append(
+                (self.max_occupation / self.meff_state[i] + accumulate_energy) /
+                (i + 1))
+        estimate_fermi_energy = np.array(estimate_fermi_energy)
         # check true Fermi energy
-        fermi_idx = np.argwhere(
-            (np.asarray(candidate_fermi_energy) - eig_val) < 0)
+        fermi_idx = np.argwhere((estimate_fermi_energy - eig_val) < 0)
         if fermi_idx.size == 0:
             raise ValueError(
                 'All energy levels are processed, but no Fermi energy exists.')
-        candidate_fermi_energy = np.asarray(candidate_fermi_energy)
-        fermi_energy = candidate_fermi_energy[fermi_idx[0] - 1]
+
+        fermi_energy = estimate_fermi_energy[fermi_idx[0] - 1]
         # Calculate populations of energy levels.
         n_state = []
         for i, eig_v in enumerate(eig_val):
-            n_sqrt = (fermi_energy -
-                      eig_v) * self.meff_state[i] / (const.hbar**2 * np.pi)
+            n_sqrt = (fermi_energy - eig_v) * self.meff_state[i]
             if n_sqrt < 0:
                 n_sqrt = 0
             n_state.append(n_sqrt**2)
@@ -83,7 +99,7 @@ class FermiStatistic:
                 csb_meff * self.integral_fermi_dirac(eig_v, f_energy, temp)
                 for eig_v, csb_meff in zip(eig_val, self.meff_state)
             ]
-            return self.max_occupation - np.sum(dist) / (const.hbar**2 * np.pi)
+            return self.max_occupation - np.sum(dist)
 
         f_energy_0k, _ = self.fermilevel_0k(eig_val, wave_function)
         # sol = optimize.root_scalar(func, x0=f_energy_0k, method='halley', **kwargs)
@@ -94,8 +110,7 @@ class FermiStatistic:
 
         self.n_states = [
             self.integral_fermi_dirac(energy, self.fermi_energy, temp) *
-            csb_meff / (const.hbar**2 * np.pi)
-            for energy, csb_meff in zip(eig_val, self.meff_state)
+            csb_meff for energy, csb_meff in zip(eig_val, self.meff_state)
         ]
 
         return self.fermi_energy, self.n_states
